a43499e9338123557bf3a1a3cb8a0dbf42025fcf
Graumenth <96440437+Graumenth@users.noreply.github.com>
2022-01-19 12:25:58 +0300

    Added missing files in the first push

---
+++ application/views/admin/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<?php $this->load->view('inc/head'); ?>
+<body>
+<?php $this->load->view('inc/header'); ?>
+<div class="container">
+	<h1>Admin</h1>
+
+</div>
+<?php $this->load->view('inc/footer'); ?>
+<?php $this->load->view('inc/scripts'); ?>
+</body>
+</html>
