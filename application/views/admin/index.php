476d6eb9d959da3010cbbcc77965290b2dae4977
Graumenth <96440437+Graumenth@users.noreply.github.com>
2022-01-19 12:36:02 +0300

    Admin controller and model

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -5,7 +5,21 @@
 <?php $this->load->view('inc/header'); ?>
 <div class="container">
 	<h1>Admin</h1>
-
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">Name</th>
+				<th scope="col">Message</th>
+				<th scope="col">Action</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Mark</td>
+				<td>Otto</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
 <?php $this->load->view('inc/footer'); ?>
 <?php $this->load->view('inc/scripts'); ?>
--- application/views/contact/index.php
+++ application/views/contact/index.php
@@ -12,8 +12,8 @@
 			<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 		</div>
 		<div class="form-group">
-			<label for="exampleInputPassword1">Password</label>
-			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			<label for="message_area">Password</label>
+			<textarea class="form-control" id="message_area" placeholder="Please write your message">
 		</div>
 		<button type="submit" class="btn btn-primary">Submit</button>
 	</form>
